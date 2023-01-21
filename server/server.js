9db5370ca990b118937875a8c82b270f82daa177
HardwinBui <buihardwin@yahoo.com>
2023-01-20 21:30:09 -0800

    server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(require("./routes/record"));
 app.use(require("./routes/blog"));
 app.use(require("./routes/post"));
+app.use(require("./routes/comment"));
 // get driver connection
 const dbo = require("./db/conn");
  
