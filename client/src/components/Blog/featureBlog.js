312432ed2f53cc27e5393d3a4dcd10fcb36cba20
HardwinBui <buihardwin@yahoo.com>
2023-01-23 02:18:49 -0800

    server replacement

--- client/src/components/Blog/featureBlog.js
+++ client/src/components/Blog/featureBlog.js
@@ -22,7 +22,7 @@ export default function RecordList() {
  // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`http://localhost:5000/blog/`);
+     const response = await fetch(`http://blogmydaybackend.onrender.com/blog/`);
  
      if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
@@ -41,7 +41,7 @@ export default function RecordList() {
  
  // This method will delete a record
  async function deleteRecord(id) {
-   await fetch(`http://localhost:5000/${id}`, {
+   await fetch(`http://blogmydaybackend.onrender.com/${id}`, {
      method: "DELETE"
    });
  
