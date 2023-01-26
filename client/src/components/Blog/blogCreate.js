e05c3726a7c178a072a6da72aa81a21e2cc584f6
HardwinBui <buihardwin@yahoo.com>
2023-01-25 20:00:16 -0800

    test2

--- client/src/components/Blog/blogCreate.js
+++ client/src/components/Blog/blogCreate.js
@@ -3,9 +3,9 @@ import { useNavigate } from "react-router";
 import { useAuth0 } from "@auth0/auth0-react";
  
 export default function BlogCreate() {
- //const { user } = useAuth0();
+ const { user } = useAuth0();
  const [form, setForm] = useState({
-   user:"",//user.email,
+   user: user.email,
    name: "",
    likes: 0,
  });
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,12 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
   <Auth0Provider
     domain="dev-gu2ural13ockmvha.us.auth0.com"
     clientId="5nARigvDlooDwUXR5FCf0pZ76HltxpG5"
